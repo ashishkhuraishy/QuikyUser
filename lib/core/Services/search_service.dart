0d1c404cc2edb71ac238fc3ef73a2ca426d68b85
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-08-29 23:19:24 +0530

    completed search

---
+++ lib/core/Services/search_service.dart
@@ -0,0 +1,19 @@
+import 'package:quiky_user/features/search/domain/use_case/get_queries.dart';
+import 'package:quiky_user/features/search/domain/use_case/get_result.dart';
+
+import '../../injection_container.dart';
+
+class SearchService {
+  GetResults _getResults = sl();
+  GetQueries _getQueries = sl();
+
+  List<String> get recentQueries => _getQueries();
+
+  getResaturents({String query, double lat, double lng}) async {
+    final result = await _getResults(query: query, lat: lat, long: lng);
+
+    // TODO ; Edit Here
+
+    result.fold((failure) => null, (restaurants) => null);
+  }
+}
