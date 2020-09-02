c60a64ad9d02f4b26bbd99d6c91759c60a17a79a
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-09-02 13:15:15 +0530

    edited home remote data source

--- lib/features/home/data/data_source/home_remote_data_source.dart
+++ lib/features/home/data/data_source/home_remote_data_source.dart
@@ -9,26 +9,27 @@ import 'package:quiky_user/features/home/domain/entity/recipie.dart';
 
 const BASE_URL = 'http://3.7.65.63';
 
+enum StoreType {
+  inTheSpotlight,
+  popularBrand,
+  trendingRestaurants,
+  trendingGroceries,
+}
+
 abstract class HomeRemoteDataSource {
   /// Get the data from the Recipies api and parse them to list
   ///
   /// on failure throw [ServerException]
   Future<List<Recipie>> getRecipies();
-  Future<List<RestaurantModel>> getFeatured({
+
+  /// Get the list of all [Restaurents] which belongs to a
+  /// specific [StoreType] which takes `lat`, `long` and the `storetype`
+  ///
+  /// throws a [ServerException] on any failed response
+  Future<List<RestaurantModel>> getStores({
     @required double lat,
     @required double long,
-  });
-  Future<List<RestaurantModel>> getPopular({
-    @required double lat,
-    @required double long,
-  });
-  Future<List<RestaurantModel>> getTrendingRestaurents({
-    @required double lat,
-    @required double long,
-  });
-  Future<List<RestaurantModel>> getTrendingGrocery({
-    @required double lat,
-    @required double long,
+    @required StoreType storeType,
   });
 }
 
@@ -51,59 +52,43 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   }
 
   @override
-  Future<List<RestaurantModel>> getFeatured({double lat, double long}) async {
-    final url = BASE_URL +
-        '/store_list/?lat=$lat&lon=$long&store_type=food&highlight_status=True';
-    return _getRestaurants(url);
-  }
-
-  @override
-  Future<List<RestaurantModel>> getPopular({double lat, double long}) async {
-    final url =
-        BASE_URL + '/store_list/?lat=$lat&lon=$long&featured_brand=True';
-    return _getRestaurants(url);
-  }
-
-  @override
-  Future<List<RestaurantModel>> getTrendingGrocery(
-      {double lat, double long}) async {
-    final url = BASE_URL +
-        '/store_list/' +
-        '?lat=$lat&lon=$long&filter=trending&store_type=grocery';
-    return _getRestaurants(url);
-  }
-
-  @override
-  Future<List<RestaurantModel>> getTrendingRestaurents(
-      {double lat, double long}) async {
-    final url = BASE_URL +
-        '/store_list/' +
-        '?lat=$lat&lon=$long&filter=trending&store_type=food';
-    return _getRestaurants(url);
-  }
-
-  Future<List<RestaurantModel>> _getRestaurants(String url) async {
+  Future<List<RestaurantModel>> getStores(
+      {double lat, double long, StoreType storeType}) async {
+    final url = _generateUrl(storeType, lat, long);
     Response response = await client.get(url);
     if (response.statusCode == 200) {
       List body = jsonDecode(response.body);
       return body
           .map<RestaurantModel>((e) => RestaurantModel.fromJson(e))
           .toList();
-    } else {
-      // _debug(response);
-      throw ServerException();
     }
+
+    /// `Un-comment these lines for debugging`
+    // print(response.statusCode);
+    // print(response.body);
+    throw ServerException();
   }
 
-  // _debug(Response response) {
-  //   print(response.statusCode);
-  //   print(response.body);
-  // }
+  String _generateUrl(StoreType storeType, double lat, double lng) {
+    String url = BASE_URL + '/store_list/?lat=$lat&lon=$lng';
 
-  /*
-    _debugResponse(Response response, String name) {
-      print('$name finished with StatusCode ${response.statusCode}');
-      print(response.body);
-    } 
-  */
+    switch (storeType) {
+      case StoreType.inTheSpotlight:
+        url += '&store_type=food&highlight_status=True';
+        break;
+      case StoreType.popularBrand:
+        url += '&featured_brand=True';
+        break;
+      case StoreType.trendingRestaurants:
+        url += '&filter=trending&store_type=food';
+        break;
+      case StoreType.trendingGroceries:
+        url += '&filter=trending&store_type=grocery';
+        break;
+      default:
+        break;
+    }
+
+    return url;
+  }
 }
