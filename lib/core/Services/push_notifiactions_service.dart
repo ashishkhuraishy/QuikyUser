1540e0551b8f51f846f009c0171d22bb388d69c3
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-09-18 14:15:39 +0530

    added new key

--- lib/core/Services/push_notifiactions_service.dart
+++ lib/core/Services/push_notifiactions_service.dart
@@ -1,21 +1,69 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:path/path.dart';
 
 class PushNotificationService {
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
 
-  init() {
-    print("Initialised");
+  void init() {
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: () => print(object));
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
+        showNotification(
+            message['notification']['title'], message['notification']['body']);
         print("onMessage: $message");
       },
-      // onBackgroundMessage: myBackgroundMessageHandler,
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
+        // Navigator.pushNamed(context, '/notify');
       },
       onResume: (Map<String, dynamic> message) async {
         print("onResume: $message");
       },
     );
+  }
+
+  Future onSelectNotification(String payload) async {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return new AlertDialog(
+          title: Text("PayLoad"),
+          content: Text("Payload : $payload"),
+        );
+      },
+    );
+  }
+
+  void showNotification(String title, String body) async {
+    await _demoNotification(title, body);
+  }
+
+  Future<void> _demoNotification(String title, String body) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channel_ID',
+      'channel name',
+      'channel description',
+      importance: Importance.Max,
+      playSound: true,
+      showProgress: true,
+      priority: Priority.High,
+      ticker: 'test ticker',
+    );
+
+    var iOSChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSChannelSpecifics);
+    await flutterLocalNotificationsPlugin
+        .show(0, title, body, platformChannelSpecifics, payload: 'test');
   }
 }
