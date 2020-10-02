0e3549711bcec5713b2cbc492b74ca5a019f5531
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-10-02 11:51:11 +0530

    final build v0.1

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 import 'Screens/home.dart';
 import 'Screens/selectlocation.dart';
 import 'core/Providers/AddressProvider.dart';
-import 'core/Services/push_notifiactions_service.dart';
 import 'features/location_service/data/data_source/address_local_data_sourc.dart';
 
 class Wrapper extends StatefulWidget {
@@ -16,7 +15,6 @@ class Wrapper extends StatefulWidget {
 class _WrapperState extends State<Wrapper> {
   @override
   Widget build(BuildContext context) {
-    PushNotificationService().init(context);
     Box addressBox = Hive.box(ADDRESS_BOX);
     if (addressBox.isEmpty)
       return SelectLocation();
